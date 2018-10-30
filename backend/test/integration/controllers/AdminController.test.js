ed3f7c7129b7956ba5790f68a6bd4412bb82b64f
JCVelazco <juan.carlosvr.97@hotmail.com>
2018-10-30 12:48:51 -0600

    add unit test, and change the database of de tests

--- backend/test/integration/controllers/AdminController.test.js
+++ backend/test/integration/controllers/AdminController.test.js
@@ -2,16 +2,29 @@ var supertest = require('supertest');
 
 describe('AdminController.login', function() {
 
-  describe('#login()', function() {
-    it('should return a 200, of login admin success', function (done) {
-      supertest(sails.hooks.http.app)
-      .post('/admin/login')
-      .send({ email: 'test@email.com', password: 'test' })
-      .expect(200).end(function(err, res) {
-        if (err) throw err;
-        done();
-      });
+    describe('#create()', function() {
+        it('should return a 200, of create admin success', function (done){
+            supertest(sails.hooks.http.app)
+            .post('/admin/create')
+            .send({ email: 'test@admin.com', password: 'test'})
+            .expect(200).end(function(err, res){
+                if(err) throw err;
+                done();
+            })
+        })
     });
-  });
+    
+    describe('#login()', function() {
+        it('should return a 200, of login admin success', function (done) {
+            supertest(sails.hooks.http.app)
+            .post('/admin/login')
+            .send({ email: 'test@admin.com', password: 'test' })
+            .expect(200).end(function(err, res) {
+                if (err) throw err;
+                done();
+            });
+        });
+    });
+    
 });
 
