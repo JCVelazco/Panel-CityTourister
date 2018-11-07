ff53c9733120cb623707baa92dc17ee97899249c
JCVelazco <juan.carlosvr.97@hotmail.com>
2018-11-07 14:27:22 -0600

    add narrative add

---
+++ backend/api/controllers/narrative/add.js
@@ -0,0 +1,42 @@
+module.exports = {
+  
+  
+  friendlyName: 'Add',
+  
+  
+  description: 'Add Narrative.',
+  
+  
+  inputs: {
+    audio_url: {
+      type: 'string',
+      unique: true,
+      required: true,
+      allowNull: false,
+      isURL: true
+    },
+  },
+  
+  
+  fn: async function (inputs, exits) {
+    
+    sails.log.info("narrative/add");
+        
+    var newNarrative = await Narrative.create({
+      isURL: inputs.isURL
+    })
+    .fetch();
+    
+    if(!newNarrative) return exits.serverError({
+      info: 'Internal server error'
+    });
+    
+    return exits.success({
+      info: 'New Narrative added',
+      id: newNarrative.id
+    });
+    
+  }
+  
+  
+};
