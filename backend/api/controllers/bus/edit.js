40682ba48d0db256909aaa4d68356d5f0f19f153
Unknown <jes.librado@gmail.com>
2018-11-09 13:02:00 -0600

    Bus edit template

--- backend/api/controllers/bus/edit.js
+++ backend/api/controllers/bus/edit.js
@@ -1,81 +1,16 @@
-module.exports = {
-  
-  
-  friendlyName: 'Edit',
-  
-  
-  description: 'Edit Bus.',
-  
-  
-  inputs: {   
-    availability: {
-      type: 'boolean',
-      required: true,
-      allowNull: false
-    },
-    
-    numBus: {
-      type: 'string',
-      required: true,
-      allowNull: false,
-      minLength: 3
-    },
-    
-    tour_id: {
-      type: 'number',
-      required: true
-    }
-  },
-  
-  exits: {
-    success: {
-      statusCode: 200,
-      description: 'Bus was edited'
-    },
-    serverError: {
-      statusCode: 500,
-      description: 'Bus could not be edited'
-    }
-  },
-  
-  
-  
-  fn: async function (req, res) {
-    
-    try {
-      
-      let params = req.params;
-      let attributes = {};
-      if (params.availability) {
-        attributes.availability = params.availability;
-      }
-      if (params.numBus) {
-        attributes.numBus = params.numBus;
-      }
-      if (params.tour_id) {
-        attributes.tour_id = params.tour_id;
-      }
-      
-      const results = await Bus.update({id: req.params.id}, attributes);
-      return res.ok(results);
-      
-    }
-    catch (err) {
-      return res.serverError(err);
-    }
-    
-    
-    
-    
-    
-    
-    
-    
-    
-  }
-  
-  
-  
-  
-  
-};
+module.exports = async (req, res) => {
+  const busId = req.param('id');
+
+  let tourObj = await tourObj.findOne({id: req.body.tour_id});
+
+  if(!tourObj) return res.json({info: 'Tour not Found'});
+
+  var updatedBus = await Bus.update({id: busId})
+  .set({
+    availability: req.body.availability,
+    numBus: req.body.numBus,
+    tour_id: tourObj
+  }).fetch();
+
+  return res.json(updatedBus);
+}
