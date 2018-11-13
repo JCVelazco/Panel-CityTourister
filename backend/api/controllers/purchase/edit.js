60a769b451e8edcfb8bf29148f0bbccc353ab4e7
JCVelazco <juan.carlosvr.97@hotmail.com>
2018-11-12 21:13:37 -0600

    add the purchase edit

---
+++ backend/api/controllers/purchase/edit.js
@@ -0,0 +1,53 @@
+module.exports = async (req, res) => {
+  const PurchaseId = req.param('id');
+  let user_id = req.body.user_id;
+  let company_id = req.body.company_id;
+  let tickets = req.body.tickets;
+  
+  
+  let currentPurchase = await Purchase.findOne({id: PurchaseId});
+  
+  if(!currentPurchase) 
+  return res.json({info: 'Purchase notFound'});
+  
+  
+  var userObj = ' ';
+  
+  if(user_id)
+  userObj = await User.findOne({id: user_id});
+  
+  if(!userObj) 
+  return res.json({info: 'User notFound'});
+
+  var companyObj = ' ';
+  
+  if(company_id)
+  companyObj = await Company.findOne({id: company_id});
+  
+  if(!companyObj) 
+  return res.json({info: 'Company notFound'});
+
+  var ticketObj = ' ';
+  
+  if(tickets)
+  ticketObj = await Ticket.findOne({id: tickets});
+  
+  if(!ticketObj) 
+  return res.json({info: 'Ticket notFound'});
+  
+  var updatedPurchase = await Purchase.update({id: PurchaseId})
+  .set({
+    sub_total: req.sub_total,
+    total: req.total,
+    user_id: userObj.id,
+    company_id: companyObj.id,
+    tickets: ticketObj.id
+  })
+  .intercept((err)=>{
+    err.message = 'An error has ocurred: '+err.message;
+    return err;
+   })
+  .fetch();
+  
+  return res.json(updatedPurchase);
+}
