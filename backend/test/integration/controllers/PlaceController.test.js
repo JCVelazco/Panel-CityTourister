899d4c0ccaf8607dcdaac5b2d3bf9e385bfecdd9
JCVelazco <juan.carlosvr.97@hotmail.com>
2018-11-22 23:06:51 -0600

    finish the unit test

--- backend/test/integration/controllers/PlaceController.test.js
+++ backend/test/integration/controllers/PlaceController.test.js
@@ -1,12 +1,15 @@
 var supertest = require('supertest');
 
-describe('PlaceController', function() {
+
+//ADD PLACETYPE, BECAUSE YOU NEED IT TO CREATE A PLACE
+describe('PlaceTypeController', function() {
   
   describe('#add()', function(){
-    it('should return a 200, of add place success', function (done){
+    it('should return a 200, of add placetype success', function (done){
       supertest(sails.hooks.http.app)
-      .post('/place')
-      .send({ name: 'place Test', description: 'descrption of test', place_type_id:'1'})
+      .post('/placetype/add')
+      .set({'auth': sails.session.token})
+      .send({ name: 'Test Ticket Place'})
       .expect(200).end(function(err, res){
         if(err) throw err;
         done();
@@ -15,11 +18,12 @@ describe('PlaceController', function() {
   })
   
   
-  describe('#patch()', function() {
-    it('should return a 200, of patch place success', function (done) {
+  describe('#put()', function() {
+    it('should return a 200, of put placetype success', function (done) {
       supertest(sails.hooks.http.app)
-      .patch('/place/1')
-      .send({ place_type_id:'2'})
+      .put('/placetype/edit/1')
+      .set({'auth': sails.session.token})
+      .send({ name: 'Test Ticket Place Edit'})
       .expect(200).end(function(err, res) {
         if (err) throw err;
         done();
@@ -27,11 +31,42 @@ describe('PlaceController', function() {
     });
   });
   
+  describe('#get()', function() {
+    it('should return a 200, of get placetype success', function (done) {
+      supertest(sails.hooks.http.app)
+      .get('/placetype/1')
+      .set({'auth': sails.session.token})
+      .expect(200).end(function(err, res) {
+        if (err) throw err;
+        done();
+      });
+    });
+  });
+});
+
+
+describe('PlaceController', function() {
+  
+  describe('#add()', function(){
+    it('should return a 200, of add place success', function (done){
+      supertest(sails.hooks.http.app)
+      .post('/place/add')
+      .set({'auth': sails.session.token})
+      .send({ name: 'place Test', description: 'descrption of test', place_type_id:'1', latitude: 15.0005, longitude: 48.5524})
+      .expect(200).end(function(err, res){
+        if(err) throw err;
+        done();
+      })
+    })
+  })
+  
+  
   describe('#put()', function() {
     it('should return a 200, of put place success', function (done) {
       supertest(sails.hooks.http.app)
-      .put('/place/1')
-      .send({ name: 'place Test2', description: 'descrption of test2', place_type_id:'3'})
+      .put('/place/edit/1')
+      .set({'auth': sails.session.token})
+      .send({ name: 'place Test edited', description: 'descrption of test edited', place_type_id:'1', latitude: 17.0005, longitude: 58.5524})
       .expect(200).end(function(err, res) {
         if (err) throw err;
         done();
@@ -43,6 +78,7 @@ describe('PlaceController', function() {
     it('should return a 200, of get place success', function (done) {
       supertest(sails.hooks.http.app)
       .get('/place/1')
+      .set({'auth': sails.session.token})
       .expect(200).end(function(err, res) {
         if (err) throw err;
         done();
@@ -54,6 +90,19 @@ describe('PlaceController', function() {
     it('should return a 200, of delete place success', function (done) {
       supertest(sails.hooks.http.app)
       .delete('/place/1')
+      .set({'auth': sails.session.token})
+      .expect(200).end(function(err, res) {
+        if (err) throw err;
+        done();
+      });
+    });
+  });
+
+  describe('#delete()', function() {
+    it('should return a 200, of delete placetype success', function (done) {
+      supertest(sails.hooks.http.app)
+      .delete('/placetype/1')
+      .set({'auth': sails.session.token})
       .expect(200).end(function(err, res) {
         if (err) throw err;
         done();
--- backend/test/integration/controllers/TicketController.test.js
+++ backend/test/integration/controllers/TicketController.test.js
@@ -1,12 +1,19 @@
-/*var supertest = require('supertest');
+var supertest = require('supertest');
 
-describe('TicketController', function() {
+//TO CREATE A TICKET YOU WILL NEED A PURCHASE AND A PRICE
+
+//PRICE
+//TO CREATE A PRICE YOU NEED A TOUR AND A TICKETTYPE
+
+//TICKETYPE CREATION:
+describe('TicketTypeController', function() {
   
   describe('#add()', function(){
-    it('should return a 200, of add ticket success', function (done){
+    it('should return a 200, of add tickettype success', function (done){
       supertest(sails.hooks.http.app)
-      .post('/ticket')
-      .send({ name: 'Ticket Test', purchase_id: '1'})
+      .post('/tickettype/add')
+      .set({'auth': sails.session.token})
+      .send({ name: 'TicketType Test'})
       .expect(200).end(function(err, res){
         if(err) throw err;
         done();
@@ -15,11 +22,187 @@ describe('TicketController', function() {
   })
   
   
-  describe('#patch()', function() {
-    it('should return a 200, of patch ticket success', function (done) {
+  describe('#put()', function() {
+    it('should return a 200, of put tickettype success', function (done) {
       supertest(sails.hooks.http.app)
-      .patch('/ticket/1')
-      .send({ name: 'Patch Test'})
+      .put('/tickettype/edit/1')
+      .set({'auth': sails.session.token})
+      .send({name: 'Test'})
+      .expect(200).end(function(err, res) {
+        if (err) throw err;
+        done();
+      });
+    });
+  });
+  
+  describe('#get()', function() {
+    it('should return a 200, of get tickettype success', function (done) {
+      supertest(sails.hooks.http.app)
+      .get('/tickettype/1')
+      .set({'auth': sails.session.token})
+      .expect(200).end(function(err, res) {
+        if (err) throw err;
+        done();
+      });
+    });
+  });
+  
+});
+
+
+//ADD TOUR
+describe('TourController', function() {
+  
+  describe('#add()', function(){
+    it('should return a 200, of add tour success', function (done){
+      supertest(sails.hooks.http.app)
+      .post('/tour/add')
+      .set({'auth': sails.session.token})
+      .send({ name: 'Tour Test', image: 'image.jpg', description: 'test descripcion'})
+      .expect(200).end(function(err, res){
+        if(err) throw err;
+        done();
+      })
+    })
+  })
+  
+  describe('#put()', function() {
+    it('should return a 200, of put tour success', function (done) {
+      supertest(sails.hooks.http.app)
+      .put('/tour/edit/1')
+      .set({'auth': sails.session.token})
+      .send({name: 'Tour Test2', image: 'url2.jpg', description: 'test descripcion 2'})
+      .expect(200).end(function(err, res) {
+        if (err) throw err;
+        done();
+      });
+    });
+  });
+  
+  describe('#get()', function() {
+    it('should return a 200, of get tour success', function (done) {
+      supertest(sails.hooks.http.app)
+      .get('/tour/1')
+      .set({'auth': sails.session.token})
+      .expect(200).end(function(err, res) {
+        if (err) throw err;
+        done();
+      });
+    });
+  });
+});
+
+
+
+//CREATE PRICE
+describe('PriceController', function() {
+  
+  describe('#add()', function(){
+    it('should return a 200, of add price success', function (done){
+      supertest(sails.hooks.http.app)
+      .post('/price/add')
+      .set({'auth': sails.session.token})
+      .send({ priceAmount: 100, ticket_type_id: '1', tour_id: '1'})
+      .expect(200).end(function(err, res){
+        if(err) throw err;
+        done();
+      });
+    });
+  });
+  
+  
+  describe('#put()', function() {
+    it('should return a 200, of put price success', function (done) {
+      supertest(sails.hooks.http.app)
+      .put('/price/edit/1')
+      .set({'auth': sails.session.token})
+      .send({priceAmount: 110, ticket_type_id: '1', tour_id: '1'})
+      .expect(200).end(function(err, res) {
+        if (err) throw err;
+        done();
+      });
+    });
+  });
+  
+  describe('#get()', function() {
+    it('should return a 200, of get price success', function (done) {
+      supertest(sails.hooks.http.app)
+      .get('/price/1')
+      .set({'auth': sails.session.token})
+      .expect(200).end(function(err, res) {
+        if (err) throw err;
+        done();
+      });
+    });
+  });
+});
+
+
+//PURCHASE
+//TO CREATE A PURCHASE YOU NEED A USER AND A COMPANY
+
+//COMPANY CREATOR
+describe('CompanyController', function() {
+  
+  describe('#add()', function(){
+    it('should return a 200, of add company success', function (done){
+      supertest(sails.hooks.http.app)
+      .post('/company/add')
+      .set({'auth': sails.session.token})
+      .send({ name: 'Test Company', full_name: 'Test Company INC', ieps: .1, iva:.15})
+      .expect(200).end(function(err, res){
+        if(err) throw err;
+        done();
+      })
+    })
+  })
+  
+  describe('#put()', function() {
+    it('should return a 200, of put company success', function (done) {
+      supertest(sails.hooks.http.app)
+      .put('/company/edit/1')
+      .set({'auth': sails.session.token})
+      .send({ name: 'Test Com', full_name: 'Test Com INC', ieps: .2, iva:.16})
+      .expect(200).end(function(err, res) {
+        if (err) throw err;
+        done();
+      });
+    });
+  });
+  
+  describe('#get()', function() {
+    it('should return a 200, of get company success', function (done) {
+      supertest(sails.hooks.http.app)
+      .get('/company/1')
+      .set({'auth': sails.session.token})
+      .expect(200).end(function(err, res) {
+        if (err) throw err;
+        done();
+      });
+    });
+  });
+});
+
+//USER CREATOR
+describe('UserController', function() {
+  
+  describe('#add()', function(){
+    it('should return a 200, of add user success', function (done){
+      supertest(sails.hooks.http.app)
+      .post('/user/add')
+      .send({ name: 'Test Prueba', email: 'test@email.com', password: 'test123'})
+      .expect(200).end(function(err, res){
+        if(err) throw err;
+        done();
+      })
+    })
+  })
+  
+  describe('#login()', function() {
+    it('should return a 200, of login user success', function (done) {
+      supertest(sails.hooks.http.app)
+      .post('/user/login')
+      .send({ email: 'test@email.com', password: 'test123' })
       .expect(200).end(function(err, res) {
         if (err) throw err;
         done();
@@ -28,10 +211,86 @@ describe('TicketController', function() {
   });
   
   describe('#put()', function() {
+    it('should return a 200, of put user success', function (done) {
+      supertest(sails.hooks.http.app)
+      .put('/user/edit/1')
+      .set({'auth': sails.session.token})
+      .send({ name: 'User Patched', email: 'put@email.com', password: '123abc' })
+      .expect(200).end(function(err, res) {
+        if (err) throw err;
+        done();
+      });
+    });
+  });
+  
+  describe('#get()', function() {
+    it('should return a 200, of get user success', function (done) {
+      supertest(sails.hooks.http.app)
+      .get('/user/1')
+      .set({'auth': sails.session.token})
+      .expect(200).end(function(err, res) {
+        if (err) throw err;
+        done();
+      });
+    });
+  });
+  
+});
+
+//PURCHASE CREATOR
+describe('PurchaseController', function() {
+  
+  describe('#add()', function(){
+    it('should return a 200, of add purchase success', function (done){
+      supertest(sails.hooks.http.app)
+      .post('/purchase/add')
+      .set({'auth': sails.session.token})
+      .send({date_tour: '1542938714', user_id: '1', company_id: '1'})
+      .expect(200).end(function(err, res){
+        if(err) throw err;
+        done();
+      })
+    })
+  })
+  
+  describe('#put()', function() {
+    it('should return a 200, of put purchase success', function (done) {
+      supertest(sails.hooks.http.app)
+      .put('/purchase/edit/1')
+      .set({'auth': sails.session.token})
+      .send({ date_tour: '1542538714', user_id: '1', company_id: '1'})
+      .expect(200).end(function(err, res) {
+        if (err) throw err;
+        done();
+      });
+    });
+  });
+  
+});
+
+
+//ticket creator
+describe('TicketController', function() {
+  
+  describe('#add()', function(){
+    it('should return a 200, of add ticket success', function (done){
+      supertest(sails.hooks.http.app)
+      .post('/ticket/add')
+      .set({'auth': sails.session.token})
+      .send({ name: 'Ticket Test', qr_code: 'a5a4s5as15a', purchase_id: '1', price_id: '1'})
+      .expect(200).end(function(err, res){
+        if(err) throw err;
+        done();
+      })
+    })
+  })
+  
+  describe('#put()', function() {
     it('should return a 200, of put ticket success', function (done) {
       supertest(sails.hooks.http.app)
-      .put('/ticket/1')
-      .send({ name: 'Ticket Test Put', purchase_id: '2'})
+      .put('/ticket/edit/1')
+      .set({'auth': sails.session.token})
+      .send({ name: 'Ticket Test edit', qr_code: 'a5a4s5ass5a', purchase_id: '1', price_id: '1'})
       .expect(200).end(function(err, res) {
         if (err) throw err;
         done();
@@ -43,6 +302,7 @@ describe('TicketController', function() {
     it('should return a 200, of get ticket success', function (done) {
       supertest(sails.hooks.http.app)
       .get('/ticket/1')
+      .set({'auth': sails.session.token})
       .expect(200).end(function(err, res) {
         if (err) throw err;
         done();
@@ -50,10 +310,95 @@ describe('TicketController', function() {
     });
   });
   
+  describe('#get()', function() {
+    it('should return a 200, of get purchase success', function (done) {
+      supertest(sails.hooks.http.app)
+      .get('/purchase/1')
+      .set({'auth': sails.session.token})
+      .expect(200).end(function(err, res) {
+        if (err) throw err;
+        done();
+      });
+    });
+  });
+
   describe('#delete()', function() {
     it('should return a 200, of delete ticket success', function (done) {
       supertest(sails.hooks.http.app)
       .delete('/ticket/1')
+      .set({'auth': sails.session.token})
+      .expect(200).end(function(err, res) {
+        if (err) throw err;
+        done();
+      });
+    });
+  });
+
+  describe('#delete()', function() {
+    it('should return a 200, of delete price success', function (done) {
+      supertest(sails.hooks.http.app)
+      .delete('/price/1')
+      .set({'auth': sails.session.token})
+      .expect(200).end(function(err, res) {
+        if (err) throw err;
+        done();
+      });
+    });
+  });
+
+  describe('#delete()', function() {
+    it('should return a 200, of delete tour success', function (done) {
+      supertest(sails.hooks.http.app)
+      .delete('/tour/1')
+      .set({'auth': sails.session.token})
+      .expect(200).end(function(err, res) {
+        if (err) throw err;
+        done();
+      });
+    });
+  });
+
+  describe('#delete()', function() {
+    it('should return a 200, of delete tickettype success', function (done) {
+      supertest(sails.hooks.http.app)
+      .delete('/tickettype/1')
+      .set({'auth': sails.session.token})
+      .expect(200).end(function(err, res) {
+        if (err) throw err;
+        done();
+      });
+    });
+  });
+
+  describe('#delete()', function() {
+    it('should return a 200, of delete company success', function (done) {
+      supertest(sails.hooks.http.app)
+      .delete('/company/1')
+      .set({'auth': sails.session.token})
+      .expect(200).end(function(err, res) {
+        if (err) throw err;
+        done();
+      });
+    });
+  });
+
+  describe('#delete()', function() {
+    it('should return a 200, of delete user success', function (done) {
+      supertest(sails.hooks.http.app)
+      .delete('/user/1')
+      .set({'auth': sails.session.token})
+      .expect(200).end(function(err, res) {
+        if (err) throw err;
+        done();
+      });
+    });
+  });
+
+  describe('#delete()', function() {
+    it('should return a 200, of delete purchase success', function (done) {
+      supertest(sails.hooks.http.app)
+      .delete('/purchase/1')
+      .set({'auth': sails.session.token})
       .expect(200).end(function(err, res) {
         if (err) throw err;
         done();
@@ -61,4 +406,3 @@ describe('TicketController', function() {
     });
   });
 });
-*/
