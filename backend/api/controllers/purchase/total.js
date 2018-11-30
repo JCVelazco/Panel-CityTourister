e77f75e59bab34c8ccd64d41469968aaef9df585
JCVelazco <juan.carlosvr.97@hotmail.com>
2018-11-30 04:11:54 -0600

    fix total value in teh model of purchase

--- backend/api/controllers/purchase/total.js
+++ backend/api/controllers/purchase/total.js
@@ -20,6 +20,18 @@ module.exports = async (req, res) => {
     var company =  await Company.findOne({id: currentPurchase.company_id});
 
     var sumOfTicketsIva = sumOfTickets + (sumOfTickets*company.iva);
+
+    var updatedPurchase = await Purchase.update({id: PurchaseId})
+    .set({
+      sub_total: sumOfTickets,
+      total: sumOfTicketsIva,
+    })
+    .intercept((err)=>{
+      err.message = 'An error has ocurred: '+err.message;
+      return err;
+     })
+    .fetch();
+
     return res.json({total: sumOfTicketsIva, subtotal: sumOfTickets});
     
     
